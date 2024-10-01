a1afaa0771f8fe8761c828bb039ad2ab0966a348
Aubin SIAHA <aubinsiaha9@gmail.com>
2024-10-01 19:12:11 +0100

    feat(module): cRUD and auth on teahers
    the APIs are available, those of sign up or create teacher, login for the teacher through the
    authentication module, update data of teacher , teachers can logout, teacher's account can be
    deleted, and the validation of data passed ha sbeen implemented
    
    BREAKING CHANGE: prima file updated

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,12 +1,30 @@
-import express from 'express';
-export const authMiddleware = (requiredPermission: string) => {
-  return (
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-  ) => {
-    // Implement your authentication logic here
-    console.log('Hello ', requiredPermission);
-    next(); // Call next() to pass control to the next middleware
-  };
+import { Request, Response, NextFunction } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+interface JwtPayloadCustom extends JwtPayload {
+  id: number;
+  email: string;
+}
+
+const SECRET_KEY = process.env.JWT_SECRET || 'your_secret_key';
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers['authorization']?.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access token required' });
+  }
+
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid token' });
+    }
+
+    req.user = decoded as JwtPayloadCustom;
+    next();
+  });
 };
--- src/routes/teacher.route.ts
+++ src/routes/teacher.route.ts
@@ -1,17 +1,21 @@
 import { Router } from 'express';
 import {
-  createTeacher,
-  getTeacher,
+  signInTeacher,
+  signUpTeacher,
   updateTeacher,
   deleteTeacher,
+  logoutTeacher,
+  getTeachers,
 } from '../controllers/teacher.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
 
 const router = Router();
 
-router.get('/', getTeacher);
-router.post('/', createTeacher);
-router.put('/:id', authMiddleware('Aubin'), updateTeacher);
-router.delete('/:id', deleteTeacher);
+router.get('/sign-in', signInTeacher);
+router.post('/sign-up', signUpTeacher);
+router.put('/update', authMiddleware, updateTeacher);
+router.delete('/delete', authMiddleware, deleteTeacher);
+router.post('/logout', authMiddleware, logoutTeacher);
+router.get('/all-teachers', getTeachers);
 
 export default router;
